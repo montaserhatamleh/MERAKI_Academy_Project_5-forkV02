9d91d35004550ecc5c8f4632c42d0484ca15d3c2
AhmadAbulkhairr <ahmad.m.abulkhair@gmail.com>
2024-07-18 16:48:17 +0300

    starte code

---
+++ backend/middleware/authorization.js
@@ -0,0 +1,9 @@
+// This function checks if the user has a permission the passed permission
+
+const authorization = (string) => {
+
+    //TODO: write your code here
+  
+};
+
+module.exports = authorization;
