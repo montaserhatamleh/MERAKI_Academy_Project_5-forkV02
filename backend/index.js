01d3b0d0139cd66cf7f42832b01ad31fd4a638df
AhmadAbulkhairr <ahmad.m.abulkhair@gmail.com>
2024-07-20 13:39:42 +0300

    few changes

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,28 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const {pool} = require("./models/db")
+const {pool} = require("./models/db");
 
 const app = express();
 const PORT = process.env.PORT;
 
+
+
 app.use(cors());
 app.use(express.json());
+
+
+
+//Routers
+const userRouter = require("./routes/users")
+const roleRouter = require("./routes/roles")
+
+
+
+app.use("/users",userRouter)
+app.use("/roles",roleRouter)
+
+
 
 // Handles any other endpoints [unassigned - endpoints]
 app.use("*", (req, res) => res.status(404).json("NO content at this path"));
---
+++ backend/routes/roles.js
@@ -0,0 +1,23 @@
+const express = require ("express")
+const roleRouter = express.Router()
+
+
+roleRouter.post("/role",)
+roleRouter.post("/permission",)
+roleRouter.post("/rolePermission",)
+
+/*('Admin'),
+('Customer'),
+('Rider'),
+('Restaurant Owner');*/
+
+/*('view_users'),
+('edit_users'),
+('delete_users'),
+('manage_restaurants'),
+('view_orders'),
+('manage_orders');*/
+
+
+
+module.exports = roleRouter
