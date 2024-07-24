327059e270074211b7aba6eb6cbdcadb4ba04a70
Rawankhattab <rawan.k.khattab@gmail.com>
2024-07-24 03:48:51 +0300

    meaningful commit

--- backend/routes/review.js
+++ backend/routes/review.js
@@ -1,8 +1,7 @@
 const express = require ("express")
 const reviewRouter = express.Router()
 
-reviewRouter.post("/", reviewsController.createReview);
-reviewRouter.get("/:id", reviewsController.getReviewById);
+
 const { getReviewsForRestaurant, getReviewById, createReview } = require('../controllers/reviewsController');
 
 reviewRouter.get('/:restaurant_id', getReviewsForRestaurant);
