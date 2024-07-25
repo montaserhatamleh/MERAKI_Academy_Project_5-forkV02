b00d935838cca82298c42985642d6e742c739ab9
AhmadAbulkhairr <ahmad.m.abulkhair@gmail.com>
2024-07-25 19:23:48 +0300

    still nothing

---
+++ frontend/src/redux/auth.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+export const auth = createSlice({
+    name: "auth",
+    initialState : {
+        token: localStorage.getItem("token") || null,
+        userId: localStorage.getItem("userId") || null,
+        isLoggedIn: localStorage.getItem("token") ? true : false,
+    },
+    reducers: {
+        setLogin: (state, action) => {
+            state.token = action.payload;
+            state.isLoggedIn = true;
+            localStorage.setItem("token", action.payload);
+        },
+        setUserId: (state, action) => {
+            state.userId = action.payload;
+            localStorage.setItem("userId", action.payload);
+        },
+        setLogout: (state) => {
+            state.token = null;
+            state.userId = null;
+            state.isLoggedIn = false;
+            localStorage.clear();
+        },
+    },
+});
+
+export const { setLogin, setLogout, setUserId } = auth.actions;
+export default auth.reducer;
