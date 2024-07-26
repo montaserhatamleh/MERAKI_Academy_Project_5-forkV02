7b1707a7b85b3bf8879c49dab204f8bf2df506d1
AhmadAbulkhairr <ahmad.m.abulkhair@gmail.com>
2024-07-26 17:32:18 +0300

    few updates

--- frontend/src/redux/auth.js
+++ frontend/src/redux/auth.js
@@ -6,22 +6,25 @@ export const auth = createSlice({
     initialState : {
         token: localStorage.getItem("token") || null,
         userId: localStorage.getItem("userId") || null,
-        isLoggedIn: localStorage.getItem("token") ? true : false,
+        role: localStorage.getItem("role")|| null,
     },
     reducers: {
-        setLogin: (state, action) => {
+        setToken: (state, action) => {
             state.token = action.payload;
-            state.isLoggedIn = true;
             localStorage.setItem("token", action.payload);
         },
         setUserId: (state, action) => {
             state.userId = action.payload;
             localStorage.setItem("userId", action.payload);
         },
+        setRole: (state, action) => {
+            state.role = action.payload;
+            localStorage.setItem("role", action.payload);
+        },
         setLogout: (state) => {
             state.token = null;
             state.userId = null;
-            state.isLoggedIn = false;
+            state.role = null;
             localStorage.clear();
         },
     },
