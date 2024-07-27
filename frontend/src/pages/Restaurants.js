c2b2b3e387f70c8eaf367d49350008722a7b9e50
montaserhatamleh <montaser.alhatamleh@gmail.com>
2024-07-27 23:42:02 +0300

    creating restaurants cards

--- frontend/src/pages/Restaurants.js
+++ frontend/src/pages/Restaurants.js
@@ -1,4 +1,16 @@
 import React, { useState, useContext, useEffect } from "react";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+  Rating,
+  Container,
+  Grid,
+} from "@mui/material";
+
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -27,7 +39,33 @@ function Restaurants() {
   }, []);
   return (
     <div>
-      {restaurants.map((elem, i) => (
+      <Container maxWidth={100}>
+        <Grid container spacing={3} margin={2} padding={2}>
+          {restaurants.map((elem) => (
+            <Grid item>
+              <Card sx={{ minWidth: 300 }}>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                    {elem.name}
+                  </Typography>
+                  <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    <img src={elem.image_url} />
+                  </Typography>
+                  <Typography variant="body2">
+                    well meaning and kindly.
+                    <br />
+                    {'"a benevolent smile"'}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Learn More</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+      {/* {restaurants.map((elem, i) => (
         <div key={i}>
           <h1>{elem.name}</h1>
           <h1>{elem.image_url}</h1>
@@ -35,7 +73,7 @@ function Restaurants() {
           <h1>{elem.rating}</h1>
           <h1>{elem.address}</h1>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
