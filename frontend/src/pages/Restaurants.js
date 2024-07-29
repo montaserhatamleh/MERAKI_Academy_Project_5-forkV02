71d84b7f27b81af2cd813f96607cff84a2ffd56e
montaserhatamleh <montaser.alhatamleh@gmail.com>
2024-07-29 03:28:13 +0300

    creating Search fun && input

--- frontend/src/pages/Restaurants.js
+++ frontend/src/pages/Restaurants.js
@@ -1,4 +1,5 @@
 import React, { useState, useContext, useEffect } from "react";
+import axios from "axios";
 import {
   Card,
   CardActions,
@@ -16,7 +17,13 @@ import {
   FormControl,
   InputLabel,
   Select,
+  Paper,
+  CardHeader,
+  IconButton,
+  InputBase,
 } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { styled, alpha } from "@mui/material/styles";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -24,17 +31,58 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
 //nav
-//params
-
-import axios from "axios";
 function Restaurants() {
   const [restaurants, setRestaurants] = useState([]);
   const [filterCategory, setFilterCategory] = useState([]);
-  // const [search, setSearch] = useState("");
+  const [search, setSearch] = useState("");
+
+  const Search = styled("div")(({ theme }) => ({
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto",
+    },
+  }));
+
+  const SearchIconWrapper = styled("div")(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  }));
+  
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: "inherit",
+    width: "100%",
+    "& .MuiInputBase-input": {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create("width"),
+      [theme.breakpoints.up("sm")]: {
+        width: "12ch",
+        "&:focus": {
+          width: "20ch",
+        },
+      },
+    },
+  }));
 
   const categorySearch = (text) => {
-
-
+    //if fetch all restaurants
+    if (text == "All") {
+      fetchAllRestaurants();
+    }
     axios
       .get(`http://localhost:5000/restaurants/byCategory/${text}`)
       .then((result) => {
@@ -46,8 +94,8 @@ function Restaurants() {
         console.log(err);
       });
   };
+
   const fetchAllRestaurants = () => {
-    
     axios
       .get("http://localhost:5000/restaurants/")
       .then((result) => {
@@ -58,11 +106,26 @@ function Restaurants() {
         console.log("fetch Restaurants not working", err);
       });
   };
+  
   useEffect(() => {
+    // function for Search 
+    // const onSearch = restaurants.filter((elem) => {
+    //   return elem.name.toLowerCase().includes(search.toLowerCase());
+    // });
+
     fetchAllRestaurants();
-  }, []);
+  }, [search, restaurants]);
   return (
     <div>
+      <Search >
+        <SearchIconWrapper>
+          <SearchIcon />
+        </SearchIconWrapper>
+        <StyledInputBase
+          placeholder="Search…"
+          inputProps={{ "aria-label": "search" }}
+        />
+      </Search>
       <FormControl
         variant="outlined"
         sx={{ mt: 4, ml: 5, width: "150px", color: "white" }}
@@ -89,6 +152,7 @@ function Restaurants() {
             categorySearch(e.target.value);
           }}
         >
+          <MenuItem value={"All"}>All</MenuItem>
           <MenuItem value={"Italian"}>Italian</MenuItem>
           <MenuItem value={"Chinese"}>Chinese</MenuItem>
           <MenuItem value={"Mexican"}>Mexican</MenuItem>
@@ -103,37 +167,41 @@ function Restaurants() {
         <Grid container spacing={15}>
           {restaurants.map((elem, i) => (
             <Grid item xs={12} sm={6} md={4} key={i}>
-              <Card sx={{ minWidth: 300, borderRadius: 2, boxShadow: 3 }}>
-                <CardContent>
-                  <Typography variant="h5" component="div" gutterBottom>
-                    {elem.name}
-                  </Typography>
-                  <div style={{ textAlign: "center" }}>
-                    <img
-                      src={elem.image_url}
-                      alt={elem.name}
-                      style={{
-                        width: "100%",
-                        height: "auto",
-                        borderRadius: "8px",
-                      }}
+              <Paper>
+                <Card sx={{ minWidth: 300, borderRadius: 2, boxShadow: 3 }}>
+                  <CardContent>
+                    <CardHeader
+                      action={<IconButton aria-label="settings"></IconButton>}
+                      title={elem.name}
+                      style={{ textAlign: "center" }}
                     />
-                  </div>
-                  <Typography
-                    variant="body2"
-                    color="text.secondary"
-                    sx={{ mt: 2 }}
-                  >
-                    <strong>Phone:</strong> {elem.phone_number}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    <strong>Rating:</strong> {elem.rating}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    <strong>Category:</strong> {elem.category}
-                  </Typography>
-                </CardContent>
-              </Card>
+                    <div style={{ textAlign: "center" }}>
+                      <img
+                        src={elem.image_url}
+                        alt={elem.name}
+                        style={{
+                          width: "200px",
+                          height: "auto",
+                          borderRadius: "8px",
+                        }}
+                      />
+                    </div>
+                    <Typography
+                      variant="body2"
+                      color="text.secondary"
+                      sx={{ mt: 2 }}
+                    >
+                      <strong>Phone:</strong> {elem.phone_number}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      <strong>Rating:</strong> {elem.rating}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      <strong>Category:</strong> {elem.category}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Paper>
             </Grid>
           ))}
         </Grid>
