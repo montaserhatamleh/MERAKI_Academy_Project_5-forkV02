f46981c104b8fd0eaace574615d3b1c990147031
AhmadAbulkhairr <ahmad.m.abulkhair@gmail.com>
2024-07-29 12:09:20 +0300

    new day

--- backend/routes/item.js
+++ backend/routes/item.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const itemRouter = express.Router();
 const {
-  getItemsByRestaurantsId,
-  updateItemsById,
-  addItemsById,
+  getItemsByRestaurantOwner,
+  updateItemById,
+  addItem,
   deleteItemById,
-  changeAvailableFromOnToOffById,
+  changeAvailability,
 } = require("../controllers/item");
 const authentication = require("../middleware/authentication");
 
 const authorization = require("../middleware/authorization");
+const multiparty = require('connect-multiparty');
 
-itemRouter.get("/getItems/:id",authentication,authorization("manage_restaurants"), getItemsByRestaurantsId);
-itemRouter.put("/updateItems/:id/:restaurant", authentication,authorization("manage_restaurants"),updateItemsById);
-itemRouter.post("/addItems/:id",authentication,authorization("manage_restaurants"), addItemsById);
+const multipartyMiddleware = multiparty();
+
+itemRouter.get("/getItems/",authentication,authorization("manage_restaurants"), getItemsByRestaurantOwner);
+itemRouter.put("/updateItems/:id/", authentication,authorization("manage_restaurants"),updateItemById);
+itemRouter.post("/addItems/",authentication,authorization("manage_restaurants"), multipartyMiddleware,addItem);
 itemRouter.put("/deleteItem/:id",authentication,authorization("manage_restaurants"), deleteItemById);
-itemRouter.put("/changeAvailable/:id",authentication,authorization("manage_restaurants"), changeAvailableFromOnToOffById);
+itemRouter.put("/changeAvailable/:id",authentication,authorization("manage_restaurants"), changeAvailability);
 module.exports = itemRouter;
+
