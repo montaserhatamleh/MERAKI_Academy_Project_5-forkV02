bf2e19b04b8f76a058a085fe3bfaabff4b482e4a
Rawankhattab <rawan.k.khattab@gmail.com>
2024-08-05 23:28:28 +0300

    footerupdated

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Box, Typography, Link, Container, Divider } from '@mui/material';
-
+import Faq from './Faq';
+import Terms from './Terms';
+import Privacy from './Privacy';
 const Footer = () => {
   return (
     <Box sx={{ backgroundColor: '#044a7e', color: 'white', padding: '40px 0', borderTop: '1px solid #333' }}>
@@ -15,20 +17,20 @@ const Footer = () => {
           </Box>
           <Box sx={{ flex: 1, minWidth: 180 }}>
             <Typography variant="h6" sx={{ fontWeight: 'bold', mb: 2 }}>Support</Typography>
-            <Link href="/faq" color="inherit" underline="hover" sx={{ display: 'block', mb: 1 }}>FAQ</Link>
-            <Link href="/privacy" color="inherit" underline="hover" sx={{ display: 'block', mb: 1 }}>Privacy Policy</Link>
-            <Link href="/terms" color="inherit" underline="hover">Terms of Service</Link>
+            <Link href="Faq" color="inherit" underline="hover" sx={{ display: 'block', mb: 1 }}>FAQ</Link>
+            <Link href="privacy" color="inherit" underline="hover" sx={{ display: 'block', mb: 1 }}>Privacy Policy</Link>
+            <Link href="terms" color="inherit" underline="hover">Terms of Service</Link>
           </Box>
           <Box sx={{ flex: 1, minWidth: 180 }}>
             <Typography variant="h6" sx={{ fontWeight: 'bold', mb: 2 }}>Contact Us</Typography>
-            <Typography variant="body2" sx={{ mb: 1 }}>1234 Street Name, City, State, 56789</Typography>
-            <Typography variant="body2" sx={{ mb: 1 }}>Email: contact@company.com</Typography>
-            <Typography variant="body2">Phone: (123) 456-7890</Typography>
+            <Typography variant="body2" sx={{ mb: 1 }}>285 Amman, Jordan, State, 56789</Typography>
+            <Typography variant="body2" sx={{ mb: 1 }}>Email: contact@feedme.com</Typography>
+            <Typography variant="body2">Phone: (962) 456-7890</Typography>
           </Box>
         </Box>
         <Divider sx={{ my: 3, backgroundColor: '#333' }} />
         <Typography variant="body2" align="center">
-          © {new Date().getFullYear()} Company Name. All rights reserved.
+          © {new Date().getFullYear()} FeedMe. All rights reserved.
         </Typography>
       </Container>
     </Box>
